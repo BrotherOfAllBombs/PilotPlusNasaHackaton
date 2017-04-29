101c56206d1a55e886b55674acdfd87b2e7a9c9f
illya.kondratov <uh13k16tfh>
2017-04-29 15:37:58 +0300

    Initial commit

---
+++ app/src/main/java/com/nasa/kiev/spaceapps/challenge/pilotplus/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nasa.kiev.spaceapps.challenge.pilotplus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
