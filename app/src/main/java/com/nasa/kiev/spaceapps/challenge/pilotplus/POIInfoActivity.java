587380755935f4af15c33b95c3ab3c0599e16268
illya.kondratov <uh13k16tfh>
2017-04-29 23:01:59 +0300

    Flinging thru the content

--- app/src/main/java/com/nasa/kiev/spaceapps/challenge/pilotplus/POIInfoActivity.java
+++ app/src/main/java/com/nasa/kiev/spaceapps/challenge/pilotplus/POIInfoActivity.java
@@ -2,7 +2,10 @@ package com.nasa.kiev.spaceapps.challenge.pilotplus;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,14 +18,16 @@ import com.nasa.kiev.spaceapps.challenge.pilotplus.model.PointOfInterest;
  *
  */
 
-public class POIInfoActivity extends AppCompatActivity {
+public class POIInfoActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     public static final String POINT = "POINT";
+    public static final int SPEED_TRIANGLE_FACTOR = 3;
     private PointOfInterest pointOfInterest;
     private ImageView image;
     private TextView text;
     private int position;
     private Button button;
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,33 +40,93 @@ public class POIInfoActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawImage(position);
+                outputData(position);
                 position++;
             }
         });
 
+        mDetector = new GestureDetectorCompat(this, this);
+
         pointOfInterest = getIntent().getExtras().getParcelable(POINT);
+
+        outputData(0);
+        position = 0;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        drawImage(0);
-        position = 1;
     }
 
-    /*public void passInfo(PointOfInterest point) {
-        this.pointOfInterest = point;
-        drawImage(0);
-        position = 1;
-    }*/
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
-    private void drawImage(int i) {
-        if (i < pointOfInterest.getDescriptions().size()) {
-            POIDescription description = pointOfInterest.getDescriptions().get(i);
-            image.setImageResource(description.getImage().getImageId());
-            text.setText(description.getText());
+    private void outputData(int i) {
+        POIDescription description = pointOfInterest.getDescriptions().get(i);
+        image.setImageResource(description.getImage().getImageId());
+        text.setText(description.getText());
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return true;
+    }
+
+    private boolean isLeftToRight(float distanceX) {
+        return distanceX > 0;
+    }
+
+    private boolean isHorizontalSwipe(float distanceX, float distanceY) {
+        return Math.abs(distanceY * SPEED_TRIANGLE_FACTOR) < Math.abs(distanceX);
+    }
+
+    private void previous() {
+        if (position > 0) {
+            position--;
+            outputData(position);
         }
+    }
+
+    private void next() {
+        if (position < pointOfInterest.getDescriptions().size() - 1) {
+            position++;
+            outputData(position);
+        }
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if (isHorizontalSwipe(velocityX, velocityY)) {
+            if (isLeftToRight(velocityX)) {
+                previous();
+            } else {
+                next();
+            }
+            return true;
+        }
+        return false;
     }
 }
