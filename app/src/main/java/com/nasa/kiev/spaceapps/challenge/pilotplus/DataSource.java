23ea7cd6e3aa93bc2f9fc68504e94426c0694479
illya.kondratov <uh13k16tfh>
2017-04-30 09:17:25 +0300

    Updating data

--- app/src/main/java/com/nasa/kiev/spaceapps/challenge/pilotplus/DataSource.java
+++ app/src/main/java/com/nasa/kiev/spaceapps/challenge/pilotplus/DataSource.java
@@ -21,11 +21,10 @@ public class DataSource {
         point = new PointOfInterest("Последнее путешествие \"Титаника\"", new LatLng(41.726931, -49.948253));
 
         List<LatLng> waypoints = new ArrayList<>();
-        waypoints.add(new LatLng(0, 0));
-        waypoints.add(new LatLng(5, -15));
-        waypoints.add(new LatLng(10, -20));
-        waypoints.add(new LatLng(20, -25));
-        waypoints.add(new LatLng(40, -45));
+        waypoints.add(new LatLng(50.8, -1.35));
+        waypoints.add(new LatLng(49.6, -1.6));//Шербур
+        waypoints.add(new LatLng(49.6, -6.5));
+        waypoints.add(new LatLng(51.8, -8.3));//Квинстаун
         waypoints.add(new LatLng(41.7, -49.95));
         point.setWaypoints(waypoints);
 
@@ -33,38 +32,51 @@ public class DataSource {
         List<LatLng> pointsToShow = new ArrayList<>();
         List<LatLng> waypointsToShow = new ArrayList<>();
 
-        pointsToShow.add(new LatLng(30, -35));
-        pointsToShow.add(new LatLng(50, -55));
-        descriptions.add(new POIDescription("Title 1", "Some description 1", new POIImage(R.drawable.image_1_1), pointsToShow, null));
+        pointsToShow.add(new LatLng(30, 0));
+        pointsToShow.add(new LatLng(50, -90));
+        descriptions.add(new POIDescription("Титаник", "\"Титаник\" - самый известный из трансатлантических пароходов, погибший в результате столкновения с айсбергом в 1912 году. Одной из причин этой катастрофы, к слову, считается рекордное сближение Земли и Луны, вызвавшее очень высокие приливы и позволившие айсбергам уже ранней весной оказаться настолько далеко к югу.", new POIImage(R.drawable.image_1_1_titanik), pointsToShow, null));
 
         pointsToShow = new ArrayList<>();
         pointsToShow.add(new LatLng(54.5, -5.8));
         pointsToShow.add(new LatLng(54.75, -6));
         descriptions.add(new POIDescription("Строительство", "Титаник строился на верфи Белфаста и был спущен на воду в 1911 году. Корпус корабля скрепляли свыше трёх миллионов заклёпок, 75% из которых были забиты вручную.", new POIImage(R.drawable.image_1_2_building), pointsToShow, null));
 
-        pointsToShow = new ArrayList<>();
-        pointsToShow.add(new LatLng(0, 0));
-        pointsToShow.add(new LatLng(5, 5));
-        descriptions.add(new POIDescription("Сердце \"Титаника\"", "«Титаник» был оборудован двумя четырехцилиндровыми паровыми машинами и паровой турбиной. Вся силовая установка обладала мощностью 55 000 л. с. Корабль мог развивать скорость до 23 узлов (42 км/ч)", new POIImage(R.drawable.image_1_3), pointsToShow, pointsToShow));
+        POIDescription descriptionBelfast = new POIDescription("Сердце \"Титаника\"", "«Титаник» был оборудован двумя четырехцилиндровыми паровыми машинами и паровой турбиной. Вся силовая установка обладала мощностью 55 000 л. с. Корабль мог развивать скорость до 23 узлов (42 км/ч).", new POIImage(R.drawable.image_1_3), pointsToShow, null);
+        descriptions.add(descriptionBelfast);
 
-        pointsToShow = new ArrayList<>();
-        pointsToShow.add(new LatLng(-3, -3));
-        pointsToShow.add(new LatLng(-30, -30));
-        descriptions.add(new POIDescription("Title 4", "Some description 4 very very very very very very very very very very very very very very very very very long", new POIImage(R.drawable.image_1_4), pointsToShow, pointsToShow));
-
-        pointsToShow = new ArrayList<>();
-        pointsToShow.add(new LatLng(30, -35));
-        pointsToShow.add(new LatLng(50, -55));
         waypointsToShow = new ArrayList<>();
-        waypointsToShow.add(new LatLng(40, -43));
-        waypointsToShow.add(new LatLng(40, -47));
-        waypointsToShow.add(new LatLng(43, -47));
-        waypointsToShow.add(new LatLng(43, -43));
-        waypointsToShow.add(new LatLng(40, -43));
-        POIDescription description = new POIDescription("Title 5", "Crash site", new POIImage(R.drawable.image_1_5), pointsToShow, waypointsToShow);
+        waypointsToShow.add(new LatLng(50.8, -1.35));
+        waypointsToShow.add(new LatLng(49.6, -1.6));//Шербур
+        waypointsToShow.add(new LatLng(49.6, -6.5));
+        waypointsToShow.add(new LatLng(51.8, -8.3));//Квинстаун
+        waypointsToShow.add(new LatLng(41.7, -49.95));
+        descriptions.add(new POIDescription("Путешествие", "10 апреля 1912 года \"Титаник\" вышел из Саусгемптона и направился в Нью-Йорк, посетив по пути Шербур (Франция) и Куинстаун (Ирландия). На борту было 1320 пассажиров, включая председателя пароходной компании-владельца Брюса Исмея.", new POIImage(R.drawable.image_1_4), waypointsToShow, waypointsToShow));
+
+        pointsToShow = new ArrayList<>();
+        pointsToShow.add(new LatLng(25, 30));
+        pointsToShow.add(new LatLng(55, -90));
+        waypointsToShow = new ArrayList<>();
+        waypointsToShow.add(new LatLng(41, -49));
+        waypointsToShow.add(new LatLng(41, -51));
+        waypointsToShow.add(new LatLng(43, -51));
+        waypointsToShow.add(new LatLng(43, -49));
+        waypointsToShow.add(new LatLng(41, -49));
+        POIDescription description = new POIDescription("Катастрофа", "14 апреля в 23:39 \"Титаник\" на полном ходу столкнулся в тумане с айсбергом. Спустя почти два часа корабль разломился и ушёл на дно.", new POIImage(R.drawable.image_1_5), pointsToShow, waypointsToShow);
         description.setBearing(270);
         description.setTilt(90);
         descriptions.add(description);
+
+        pointsToShow = new ArrayList<>();
+        pointsToShow.add(new LatLng(25, 0));
+        pointsToShow.add(new LatLng(55, -60));
+        waypointsToShow = new ArrayList<>();
+        waypointsToShow.add(new LatLng(41, -49.5));
+        waypointsToShow.add(new LatLng(41, -50.5));
+        waypointsToShow.add(new LatLng(42, -50.5));
+        waypointsToShow.add(new LatLng(42, -49.5));
+        waypointsToShow.add(new LatLng(41, -49.5));
+        POIDescription descriptionRust = new POIDescription("Обнаружение", "Обломки \"Титаника\" были обнаружены в 1985 году экспедицией Эдвина Балларда. Две половины корпуса лежат на расстоянии в 600 метров друг от друга. \nЕжегодно одна тонна металла \"Титаника\" превращается в ржавчину.", new POIImage(R.drawable.image_1_6_bow), pointsToShow, waypointsToShow);
+        descriptions.add(descriptionRust);
 
         point.setDescriptions(descriptions);
 
